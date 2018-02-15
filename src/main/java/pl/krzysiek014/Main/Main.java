92215d90b7bc1809b8572db0bb03d86804d3553c
Krzysiek014 <krzysiek014@o2.pl>
2018-02-16 00:55:48 +0100

    Initial commit

---
+++ src/main/java/pl/krzysiek014/Main/Main.java
@@ -0,0 +1,18 @@
+package pl.krzysiek014.Main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Krzysiek014 on 16.02.2018.
+ */
+public class Main extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+}
