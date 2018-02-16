19e5563084f21fbb01fcf257d1eed2e2721ef90b
Krzysiek014 <krzysiek014@o2.pl>
2018-02-16 13:26:26 +0100

    Added add button in WindowStyle and a Controller class for that window

---
+++ src/main/java/pl/krzysiek014/Controllers/ControllerWindowStyle.java
@@ -0,0 +1,10 @@
+package pl.krzysiek014.Controllers;
+
+/**
+ * Created by Krzysiek014 on 16.02.2018.
+ */
+public class ControllerWindowStyle {
+    public void initialize(){
+        
+    }
+}
--- src/main/java/pl/krzysiek014/Main/Main.java
+++ src/main/java/pl/krzysiek014/Main/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -20,10 +20,11 @@ public class Main extends Application {
 
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource("WindowStyle.fxml"));
-        ScrollPane sp = loader.load();
-        Scene scene = new Scene(sp);
+        AnchorPane ap = loader.load();
+        Scene scene = new Scene(ap);
         primaryStage.setScene(scene);
         primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.setResizable(false);
         primaryStage.show();
         positioning(primaryStage);
     }
@@ -32,6 +33,6 @@ public class Main extends Application {
         Rectangle2D screen = Screen.getPrimary().getBounds();
         System.out.println(ps.getWidth());
         ps.setX(screen.getMaxX()-ps.getWidth()+6); //Adding 6 because of scroll
-        ps.setY(screen.getMaxY()-ps.getHeight()-(screen.getMaxY()*0.03)); // Multiplying by 0.03 because of taskbar
+        ps.setY(screen.getMaxY()-ps.getHeight()-38); // Subtract 38 because of taskbar
     }
 }
