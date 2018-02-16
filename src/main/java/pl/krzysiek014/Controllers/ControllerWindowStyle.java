ef601167b2dadc6fd96e94316f1bc748bb18a564
Krzysiek014 <krzysiek014@o2.pl>
2018-02-16 14:39:38 +0100

    Added AddStreamer.fxml

--- src/main/java/pl/krzysiek014/Controllers/ControllerWindowStyle.java
+++ src/main/java/pl/krzysiek014/Controllers/ControllerWindowStyle.java
@@ -1,7 +1,13 @@
 package pl.krzysiek014.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import pl.krzysiek014.GoogleApi.GoogleApi;
 
 import java.io.IOException;
@@ -13,9 +19,25 @@ public class ControllerWindowStyle {
 
     @FXML
     VBox wall;
+    @FXML
+    Button addButton;
 
 
     public void initialize() throws IOException {
         GoogleApi api = new GoogleApi();
+
+        addButton.setOnMouseClicked(e->{
+            Stage addWindow = new Stage();
+            FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource("AddStreamer.fxml"));
+            try {
+                AnchorPane ap = loader.load();
+                Scene scene = new Scene(ap);
+                addWindow.setScene(scene);
+                addWindow.setResizable(false);
+                addWindow.initStyle(StageStyle.UTILITY);
+                addWindow.show();
+            } catch (IOException e1) {}
+        });
+
     }
 }
