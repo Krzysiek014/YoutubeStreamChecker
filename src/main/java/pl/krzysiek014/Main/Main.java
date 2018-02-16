34adf3837bab39a2b6ed966077471c6ec5eb2c81
Krzysiek014 <krzysiek014@o2.pl>
2018-02-16 01:12:52 +0100

    Added WindowStyle.fxml and implemented in Main

--- src/main/java/pl/krzysiek014/Main/Main.java
+++ src/main/java/pl/krzysiek014/Main/Main.java
@@ -1,7 +1,11 @@
 package pl.krzysiek014.Main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * Created by Krzysiek014 on 16.02.2018.
@@ -13,6 +17,11 @@ public class Main extends Application {
     }
 
     public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource("WindowStyle.fxml"));
+        ScrollPane sp = loader.load();
+        Scene scene = new Scene(sp);
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.UTILITY);
         primaryStage.show();
     }
 }
