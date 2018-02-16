a90c4e64c72428dfd8b18da0c150800d76bb187b
Krzysiek014 <krzysiek014@o2.pl>
2018-02-16 01:25:14 +0100

    Added positioning function in Main

--- src/main/java/pl/krzysiek014/Main/Main.java
+++ src/main/java/pl/krzysiek014/Main/Main.java
@@ -2,8 +2,10 @@ package pl.krzysiek014.Main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -23,5 +25,13 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.initStyle(StageStyle.UTILITY);
         primaryStage.show();
+        positioning(primaryStage);
+    }
+
+    private void positioning(Stage ps){
+        Rectangle2D screen = Screen.getPrimary().getBounds();
+        System.out.println(ps.getWidth());
+        ps.setX(screen.getMaxX()-ps.getWidth()+6); //Adding 6 because of scroll
+        ps.setY(screen.getMaxY()-ps.getHeight()-(screen.getMaxY()*0.03)); // Multiplying by 0.03 because of taskbar
     }
 }
