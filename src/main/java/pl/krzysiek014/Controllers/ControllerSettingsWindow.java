4b862f72f9b9ee025985c750bc8a0a10558f1633
Krzysiek014 <krzysiek014@o2.pl>
2018-02-24 14:06:23 +0100

    Add -> saveButton functionality

--- src/main/java/pl/krzysiek014/Controllers/ControllerSettingsWindow.java
+++ src/main/java/pl/krzysiek014/Controllers/ControllerSettingsWindow.java
@@ -1,8 +1,12 @@
 package pl.krzysiek014.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import pl.krzysiek014.Main.Settings;
+
+import java.io.IOException;
 
 /**
  * Created by Krzysiek014 on 24.02.2018.
@@ -10,10 +14,22 @@ import javafx.scene.control.CheckBox;
 public class ControllerSettingsWindow {
 
     @FXML
-    CheckBox showOffline, hideButtons, enableAuto;
+    private CheckBox showOffline, hideButtons, enableAuto;
     @FXML
-    Button saveButton, cancelButton;
+    private Button saveButton, cancelButton;
     public void initialize(){
+
+        saveButton.setOnAction(e->{
+            Settings.getInstance().setProperty("show-offline",Boolean.toString(showOffline.isSelected()));
+            Settings.getInstance().setProperty("hide-buttons",Boolean.toString(hideButtons.isSelected()));
+            Settings.getInstance().setProperty("auto-refresh",Boolean.toString(enableAuto.isSelected()));
+            try {
+                Settings.getInstance().saveProperties();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            ((Node) e.getSource()).getScene().getWindow().hide();
+        });
 
     }
 }
